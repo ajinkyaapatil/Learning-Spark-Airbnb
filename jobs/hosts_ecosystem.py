92c417b345687967a802c102717daede5eaaf231
Ajinkya Patil <ajinkyaarvindp@sahaj.ai>
2026-01-19 12:44:54 +0530

    refactor host ecosystems

--- jobs/hosts_ecosystem.py
+++ jobs/hosts_ecosystem.py
@@ -1,8 +1,8 @@
-from pyspark.sql.functions import udf, col, regexp_replace, mean, mode
+import plotly.express as px
+from pyspark.sql.functions import col, mean, mode, regexp_replace, udf
 from pyspark.sql.types import StringType
 
 from jobs.read_data import read_parquet_data
-from jobs.write import write_csv
 
 
 def check_host_acceptance_across_cities():
@@ -12,48 +12,58 @@ def check_host_acceptance_across_cities():
         lambda text: text.replace("%", "").replace("N/A", "0"), StringType()
     )
 
-    host_acceptance_rate = df.withColumn("host_acceptance_rate", replace_percentage(col("host_acceptance_rate"))) \
-        .withColumn("host_acceptance_rate", col("host_acceptance_rate").cast("int"))
-
-    host_response_rate = host_acceptance_rate.withColumn("host_response_rate",
-                                                         replace_percentage(col("host_response_rate"))) \
-        .withColumn("host_response_rate", col("host_response_rate").cast("int"))
-
-    host_response_time = host_response_rate.withColumn(
-        "host_response_time", regexp_replace("host_response_time", "N/A", "72")
-    ) \
-        .withColumn(
-        "host_response_time",
-        regexp_replace("host_response_time", "within an hour", "1"),
-    ) \
-        .withColumn(
-        "host_response_time",
-        regexp_replace("host_response_time", "a few days or more", "48"),
-    ) \
-        .withColumn(
-        "host_response_time",
-        regexp_replace("host_response_time", "within a day", "24"),
-    ) \
-        .withColumn(
-        "host_response_time",
-        regexp_replace("host_response_time", "within a few hours", "6"),
-    )
-
     selected_columns = (
-        host_response_time
+        df.withColumn(
+            "host_acceptance_rate", replace_percentage(col("host_acceptance_rate"))
+        )
+        .withColumn("host_acceptance_rate", col("host_acceptance_rate").cast("int"))
+        .withColumn("host_response_rate", replace_percentage(col("host_response_rate")))
+        .withColumn("host_response_rate", col("host_response_rate").cast("int"))
+        .withColumn(
+            "host_response_time", regexp_replace("host_response_time", "N/A", "72")
+        )
+        .withColumn(
+            "host_response_time",
+            regexp_replace("host_response_time", "within an hour", "1"),
+        )
+        .withColumn(
+            "host_response_time",
+            regexp_replace("host_response_time", "a few days or more", "48"),
+        )
+        .withColumn(
+            "host_response_time",
+            regexp_replace("host_response_time", "within a day", "24"),
+        )
+        .withColumn(
+            "host_response_time",
+            regexp_replace("host_response_time", "within a few hours", "6"),
+        )
         .groupby("city")
         .agg(
             mean("host_acceptance_rate").alias("avg_host_acceptance_rate"),
             mean("host_response_rate").alias("avg_host_response_rate"),
-            mode("host_response_time").alias("mode_host_response_time"),
+            mean("host_response_time").alias("mode_host_response_time"),
         )
-        .withColumn("host_ecosystem", col("avg_host_response_rate") * col("avg_host_acceptance_rate") / 10000)
-        .orderBy("host_ecosystem")
     )
 
-    selected_columns.plot.line("city", "host_ecosystem").write_image("./output/images/hosts/host_ecosystem.png")
+    px.line(
+        selected_columns,
+        "city",
+        ["avg_host_acceptance_rate", "avg_host_response_rate"],
+        title="Host Accept",
+    ).update_layout(yaxis_title="Percentage").write_image(
+        "./output/images/hosts/host_acceptance_and_response.png"
+    )
 
-    write_csv(selected_columns, path="./output/host_acceptance")
+    px.line(
+        selected_columns,
+        "city",
+        ["mode_host_response_time"],
+        title="Host Accept",
+    ).update_layout(yaxis_title="Hours").write_image(
+        "./output/images/hosts/host_response_time.png"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     check_host_acceptance_across_cities()
