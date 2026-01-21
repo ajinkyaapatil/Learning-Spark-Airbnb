e6228a64850387250dda4280c10a1af6ee7b0591
Vaibhav Vijay Nayak <vaibhavvijayn@sahaj.ai>
2026-01-21 07:13:33 +0530

    add sampling ratio parameter while reading csv

--- jobs/read_data.py
+++ jobs/read_data.py
@@ -2,8 +2,8 @@ from pyspark.sql import DataFrame
 
 from config.spark_config import spark
 
-def read_csv_data(path : str) -> DataFrame :
-    return spark.read.csv(path, header=True, inferSchema=True, quote='"', escape='"', multiLine=True)
+def read_csv_data(path : str, sr: float = 0.1) -> DataFrame :
+    return spark.read.csv(path, header=True, inferSchema=True, quote='"', escape='"', multiLine=True, samplingRatio=sr)
 
 def read_parquet_data(path: str) -> DataFrame :
     return spark.read.parquet(path)
